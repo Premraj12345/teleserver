186cd14238cdf6e56994ac0f8bbe404f571c2e80
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2024-02-23 04:30:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import os
-from telethon.sync import TelegramClient
+import requests
 from flask import Flask, send_file
+import io
 
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
 
 app = Flask(__name__)
 
 @app.route('/stream/<file_id>')
 def stream_file(file_id):
-    with TelegramClient('anon', api_id, api_hash) as client:
-        message = client.get_messages('@your_channel_username', ids=int(file_id))
-        file_path = client.download_media(message.media, file='./downloads/')
-        return send_file(file_path)
+    url = f"https://api.telegram.org/bot{bot_token}/getFile?file_id={file_id}"
+    file_path = requests.get(url).json()['result']['file_path']
+
+    # Construct the direct download link
+    download_link = f'https://api.telegram.org/file/bot{bot_token}/{file_path}'
+
+    response = requests.get(download_link)
+
+    return send_file(io.BytesIO(response.content), as_attachment=True,
+                         download_name=file_path)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
